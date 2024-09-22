439d74dc0dbd162e92e3cc6a5eabe08f689ab76f
mmusaev <9410421+mmusaev@users.noreply.github.com>
2024-09-22 17:50:33 -0500

    workding solution

---
+++ CertHelper.cs
@@ -0,0 +1,22 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace WcfFileClient
+{
+    public class CertHelper
+    {
+        public static async Task<X509Certificate2> GetAsync()
+        {
+            var keyVaultUrl = "https://kvmm.vault.azure.net/";
+            var certificateName = "localhost";
+            var client = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
+            KeyVaultSecret secret = await client.GetSecretAsync(certificateName);
+            var bytes = Convert.FromBase64String(secret.Value);
+            // Specify X509KeyStorageFlags
+            return new X509Certificate2(bytes, (string)null, X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.Exportable);
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using WcfFileClient.ServiceReference1;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace WcfFileClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly string UploadFileName = ConfigurationManager.AppSettings["UploadFileName"];
+        private readonly string DownloadFileName = ConfigurationManager.AppSettings["DownloadFileName"];
+        private readonly string DownloadedFilePath = ConfigurationManager.AppSettings["DownloadedFilePath"];
+
+        public ActionResult Index()
+        {
+            // Create a client to consume the WCF service
+            FileServiceClient client = new FileServiceClient();
+            try
+            {
+                // Call the ListFiles operation
+                var fileList = client.ListFiles();
+
+                // Pass the file list to the view
+                return View(fileList);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = "An error occurred: " + ex.Message;
+                return View();
+            }
+            finally
+            {
+                if (client != null)
+                {
+                    client.Close();
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                // Convert HttpPostedFileBase to a Stream
+                var fileUploadMessage = new FileUploadMessage
+                {
+                    FileName = file.FileName,
+                    FileData = file.InputStream
+                };
+                FileServiceClient client = new FileServiceClient();
+
+                try
+                {
+                    // Upload the file
+                    client.UploadFile(file.FileName, fileUploadMessage.FileData);
+                    ViewBag.Message = "File uploaded successfully!";
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Error = "An error occurred: " + ex.Message;
+                }
+                finally
+                {
+                    if (client != null)
+                    {
+                        client.Close();
+                    }
+                }
+            }
+            else
+            {
+                ViewBag.Error = "No file selected.";
+            }
+
+            return View("Index");
+        }
+
+        public ActionResult Download(string fileName)
+        {
+            FileServiceClient client = new FileServiceClient();
+            //client.ClientCredentials.ClientCertificate.Certificate = await CertHelper.GetAsync();
+            try
+            {
+                // Call the DownloadFile operation
+                Stream fileStream = client.DownloadFile(fileName);
+
+                return File(fileStream, "application/octet-stream", fileName);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = "An error occurred: " + ex.Message;
+                return View("Index");
+            }
+            finally
+            {
+                if (client != null)
+                {
+                    client.Close();
+                }
+            }
+        }
+    }
+}
+
